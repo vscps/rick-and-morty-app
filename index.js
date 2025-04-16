afc48e5d76b339ee995160a7703743ba835b6f44
Christian <chris@polarchris.com>
2025-04-16 11:00:49 +0200

    Implemented API fetching

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchData(url) {
+  //fetch data from API
+  const response = await fetch(url); //returns promise
+  const data = await response.json(); //take response and convert it from json to object
+  return data;
+}
+
+//fetching object and saving key results only in a new variable
+const { results: characters } = await fetchData(
+  "https://rickandmortyapi.com/api/character"
+);
+const { results: locations } = await fetchData(
+  "https://rickandmortyapi.com/api/location"
+);
+const { results: episodes } = await fetchData(
+  "https://rickandmortyapi.com/api/episode"
+);
+
+console.log(characters, locations, episodes);
